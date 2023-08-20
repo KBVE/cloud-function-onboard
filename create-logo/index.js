23034fd8d1aabcb2fccb9bab48115ea1e1ce409a
BChip <chippi.bradley@gmail.com>
2023-08-20 15:21:23 -0400

    create-logo

--- create-logo/index.js
+++ create-logo/index.js
@@ -9,9 +9,12 @@ payload:
 }
 
 */
-
+const sdk = require("node-appwrite");
 
 module.exports = async (req, res) => {
+    const client = new sdk.Client();
+    const database = new sdk.Databases(client);
+    const users = new sdk.Users(client);
     const payload = JSON.parse(req.payload);
     if (
         typeof payload.businessName === "string" &&
@@ -27,10 +30,28 @@ module.exports = async (req, res) => {
 
         const response = await fetch(url, options);
         const text = await response.text();
-        res.send(text);
+
+        // Get information about the user who triggered the function
+        const userId = req.variables["APPWRITE_FUNCTION_USER_ID"];
+
+        const user = await users.get(userId);
+
+        // Create a new profile document in the "profiles" collection
+        const logo = await database.createDocument(
+            "rentearth-dev",
+            "Logo",
+            "unique()",
+            {
+                url: url,
+                created_at: (new Date(Date.now())).toISOString(),
+                created_by: user.email
+            }
+        );
+
+        return res.json(logo);
     } else {
         // Some properties are missing, have incorrect types, or are empty strings
-        res.send("Bad Request", 400);
+        return res.json({ error: "Bad Request" }, 400);
     }
 };
 
